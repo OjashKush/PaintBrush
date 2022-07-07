e82b7231c2d1b87a28ea8e5eea8b328e8ff38ee1
Ojash Kushwaha <ojashkush@gmail.com>
2022-07-07 18:01:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let drawing = false;
+ctx.lineWidth = 0.4;
+ctx.fillStyle = '#FFCCCC';
+ctx.strokeStyle = '#A0A0A0';
+ctx.shadowOffsetX = 0;
+ctx.shadowOffsetY = 10;
+ctx.shadowBlur = 30;
+ctx.shadowColor = 'rgba(0,0,100,0.2)';
+ctx.globalCompositeOperation = 'destination-over';
+
+//grows at random speed and move in random direction
+class Root {
+    //growth points set as cursor
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.speedX = Math.random() * 4 - 2;
+        this.speedY = Math.random() * 4 - 2;
+        this.maxSize = Math.random() * 7 + 20;
+        this.size = Math.random() * 1 + 2;
+        this.vs = Math.random() * 0.2 + 0.5;
+        this.angleX = Math.random() * 6.2;
+        this.vax = Math.random() * 0.6 - 0.3;
+        this.angleY = Math.random() * 6.2;
+        this.vay = Math.random() * 0.6 - 0.3;
+        this.lightness = 10;
+        this.va = Math.random() * 0.02 + 0.05;
+        this.angle = 0;
+    }
+    //request animation frame method - each root to have its own separate loop
+    update(){
+        //increase x & y cordinate
+        this.x += this.speedX + Math.sin(this.angleX);
+        this.y += this.speedY + Math.sin(this.angleY);
+        //size is increasinng
+        this.size += this.vs;
+        //increase angle
+        this.angleX += this.vax;
+        this.angleY += this.vay;
+        this.angle += this.va;
+        //increase limit condition for lightness
+        if (this.lightness < 70) this.lightness += 0.25;
+        if (this.size < this.maxSize){
+            ctx.save();
+            ctx.translate(this.x, this.y);
+            ctx.rotate(this.angle);
+            ctx.fillRect(0, 0, this.size, this.size);
+            let double = this.size *2;
+            ctx.strokeRect(0 - double/2, 0 - double/2, double, double);
+            requestAnimationFrame(this.update.bind(this));  
+            ctx.restore();
+        }
+    }
+}
+
+window.addEventListener('mousemove', function(e){
+    if (drawing){
+    for (let i=0; i < 3; i++){
+        const root = new Root(e.x, e.y);
+        root.update();
+    }
+   }
+});
+
+window.addEventListener('mousedown', function(e){
+    drawing = true;
+    for (let i=0; i<30; i++){
+        const root = new Root(e.x, e.y);
+        root.update();
+    }
+});
+
+window.addEventListener('mouseup', function(){
+    drawing = false;
+});
